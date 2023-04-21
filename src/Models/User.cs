009863f87de51fdb849295dba3afe47acdb9c94a
Yann Hagege <yhagege@d-edge.com>
2023-04-21 17:13:26 +0200

    feat: add addItems branch

---
+++ src/Models/User.cs
@@ -0,0 +1,11 @@
+namespace Dot.Net.WebApi.Models
+{
+    public class User
+    {
+        public int Id;
+        public string Username;
+        public string Password;
+        public string Fullname;
+        public string? Role;
+    }
+}
